6121af99cbda88719f36112b8345438ee26ed2f7
raguilera <raguilera@192.168.3.1>
2010-11-16 12:21:38 +0100

    Implementado el aceptar tarjeta

---
+++ src/com/tdd/jcajero/ATM.java
@@ -0,0 +1,15 @@
+package com.tdd.jcajero;
+
+public class ATM {
+	
+	private Card card;
+	
+	public ATM(Card card){
+		this.card = card;
+	}
+	
+	public boolean acceptCard(){
+		return true;
+	}
+
+}
