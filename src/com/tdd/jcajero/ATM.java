12874201c9907ff61b413cd8df7e3d876838f401
alberto <alberto.rodriguez@biko2.com>
2010-11-16 13:43:44 +0100

    Añadida posibilidad de consultar efectivo en el cajero

--- src/com/tdd/jcajero/ATM.java
+++ src/com/tdd/jcajero/ATM.java
@@ -3,13 +3,19 @@ package com.tdd.jcajero;
 public class ATM {
 	
 	private Card card;
+	private final Amount initialATMAmount;
 	
-	public ATM(Card card){
+	public ATM(Card card, Amount initialATMAmount) {
 		this.card = card;
+		this.initialATMAmount = initialATMAmount;
 	}
-	
+
 	public boolean acceptCard(){
 		return true;
+	}
+
+	public Amount availableAmount() {
+		return initialATMAmount;
 	}
 
 }
--- test/com/tdd/jcajero/ATMTest.java
+++ test/com/tdd/jcajero/ATMTest.java
@@ -1,16 +1,29 @@
 package com.tdd.jcajero;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ATMTest {
+	private Card card;
+	private Amount initialATMAmount;
+	private ATM atm;
 	
+	@Before
+	public void initATM() {
+		card = new Card("Alicia");
+		initialATMAmount = new Amount(1000);
+		atm = new ATM(card, initialATMAmount);
+	}
+
 	@Test
 	public void acceptCardTest(){
-		Card card = new Card("Alicia");
-		
-		ATM atm = new ATM(card);
 		assertTrue(atm.acceptCard());
+	}
+
+	@Test
+	public void canRequestAvailableAmount() {
+		assertEquals(initialATMAmount, atm.availableAmount());
 	}
 
 }
