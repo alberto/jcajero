1086f53703b635e6d65353351f24466a59fa62fc
alberto <alberto.rodriguez@biko2.com>
2010-11-16 17:00:12 +0100

    Eliminados ficheros orig

--- test/com/tdd/jcajero/AccountTest.java
+++ test/com/tdd/jcajero/AccountTest.java
@@ -1,9 +1,5 @@
 package com.tdd.jcajero;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
