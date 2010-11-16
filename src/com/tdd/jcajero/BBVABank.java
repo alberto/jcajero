9a6e0e0a06372ac4ecdf8977adca9ab94bd084e2
Javier Acero <j4cegu@gmail.com>
2010-11-16 16:33:50 +0100

    Operacion para acceder a la cuenta

--- src/com/tdd/jcajero/BBVABank.java
+++ src/com/tdd/jcajero/BBVABank.java
@@ -2,4 +2,10 @@ package com.tdd.jcajero;
 
 public class BBVABank implements Bank {
 
+	@Override
+	public Account accessAcount(Card card, PIN pin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/com/tdd/jcajero/Bank.java
+++ src/com/tdd/jcajero/Bank.java
@@ -2,4 +2,6 @@ package com.tdd.jcajero;
 
 public interface Bank {
 
+	Account accessAcount(Card card, PIN pin);
+
 }
