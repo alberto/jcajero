6ffa7535dc74a3f341e26ef846755ad155a35218
Javier Acero <j4cegu@gmail.com>
2010-11-16 17:00:44 +0100

    BBVABank

--- src/com/tdd/jcajero/BBVABank.java
+++ src/com/tdd/jcajero/BBVABank.java
@@ -2,6 +2,10 @@ package com.tdd.jcajero;
 
 public class BBVABank implements Bank {
 
+	public BBVABank(BankConnector connector) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public Account accessAcount(Card card, PIN pin) {
 		// TODO Auto-generated method stub
