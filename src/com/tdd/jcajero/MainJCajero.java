2d3c848a9882491bc31b787fc4d90c9e2a3ab3f3
alberto <alberto.rodriguez@biko2.com>
2010-11-16 19:07:13 +0100

    Añadida compraración entre amounts

--- src/com/tdd/jcajero/MainJCajero.java
+++ src/com/tdd/jcajero/MainJCajero.java
@@ -67,7 +67,7 @@ public class MainJCajero {
 						}
 						
 					} catch (NumberFormatException e) {
-						System.out.println("No seas listo y mete un nœmero ;)");
+						System.out.println("No seas listo y mete un numero ;)");
 					}
 
 				} while (operationIndex != 0);
